1eb12e6b5fb23411d41166294dc36437dce89444
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-10 18:28:27 +0530

    Adding & Catching Path variables using @PathVariable

--- src/main/java/rtg/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/rtg/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package rtg.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,11 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Be Creative");
 	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
 }
+
 
