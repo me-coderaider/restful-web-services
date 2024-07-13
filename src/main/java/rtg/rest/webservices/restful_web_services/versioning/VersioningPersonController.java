289ecc08a1c3b3465ca2b8f5b7239e714f5439fe
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-13 12:02:05 +0530

    Versioning: Accept Headers

--- src/main/java/rtg/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/rtg/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -25,4 +25,25 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionOfPersonRequestParameter() {
 		return new PersonV2(new Name("John", "Doe"));
 	}
+	
+	@GetMapping(path="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("John Doe");
+	}
+	
+	@GetMapping(path="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("John", "Doe"));
+	}
+	
+	//mime-type = type "/" [tree "."] subtype ["+" suffix]* [";" parameter];
+	@GetMapping(path="/person/accept", produces="application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+		return new PersonV1("John Doe");
+	}
+	@GetMapping(path="/person/accept", produces="application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+		return new PersonV2(new Name("John", "Doe"));
+	}
 }
+
