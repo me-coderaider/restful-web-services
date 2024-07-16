6ae8509f636da73b49c55e6e20a731f34c43df30
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-16 11:26:16 +0530

    UserEntity connected to H2 Database through UserJPAResource

---
+++ src/main/java/rtg/rest/webservices/restful_web_services/jpa/UserRepository.java
@@ -0,0 +1,9 @@
+package rtg.rest.webservices.restful_web_services.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rtg.rest.webservices.restful_web_services.user.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+}
--- src/main/java/rtg/rest/webservices/restful_web_services/user/User.java
+++ src/main/java/rtg/rest/webservices/restful_web_services/user/User.java
@@ -4,10 +4,16 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name="user_details")
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=3, message="Name should have atleast 3 characters.")
@@ -17,6 +23,8 @@ public class User {
 	@Past(message="Birthdate should be in the past.")
 	@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	public User() {}
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
